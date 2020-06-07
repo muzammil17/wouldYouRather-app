2ea00f40648cbf2cb6415b970a76612075ef9bb2
muzammil17 <muzammilmajeed17@gmail.com>
2020-06-07 16:54:58 +0500

    app build

---
+++ build/precache-manifest.2cfe53724ca9a90168f15116540918d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2e4bf5e92a2603233fa80c6cfd4a87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cba1a9199601519af302",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "3067c6eda42222ab828c",
+    "url": "/static/css/main.76f5435f.chunk.css"
+  },
+  {
+    "revision": "cba1a9199601519af302",
+    "url": "/static/js/2.425d3e08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.425d3e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3067c6eda42222ab828c",
+    "url": "/static/js/main.f156be64.chunk.js"
+  },
+  {
+    "revision": "bf29f63f98c3b1cb0f16",
+    "url": "/static/js/runtime-main.19b5b27b.js"
+  },
+  {
+    "revision": "1fc43d1e2ed957b90d79b4ddd8f9d889",
+    "url": "/static/media/udacity.1fc43d1e.png"
+  }
+]);
