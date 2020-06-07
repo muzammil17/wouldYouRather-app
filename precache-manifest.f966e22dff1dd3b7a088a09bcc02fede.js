fed7c0dd9463d2ae761dc6e43c10cc92e2ba9fe2
muzammil17 <muzammilmajeed17@gmail.com>
2020-06-07 17:07:24 +0500

    Updates

---
+++ precache-manifest.f966e22dff1dd3b7a088a09bcc02fede.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7181c5245a55dd57dfd630fceea532a6",
+    "url": "/wouldYouRather-app/index.html"
+  },
+  {
+    "revision": "cba1a9199601519af302",
+    "url": "/wouldYouRather-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1380a718daa576eb0b0b",
+    "url": "/wouldYouRather-app/static/css/main.76f5435f.chunk.css"
+  },
+  {
+    "revision": "cba1a9199601519af302",
+    "url": "/wouldYouRather-app/static/js/2.425d3e08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/wouldYouRather-app/static/js/2.425d3e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1380a718daa576eb0b0b",
+    "url": "/wouldYouRather-app/static/js/main.018436d8.chunk.js"
+  },
+  {
+    "revision": "f2282f0cfb75a40de44b",
+    "url": "/wouldYouRather-app/static/js/runtime-main.d601f95c.js"
+  },
+  {
+    "revision": "1fc43d1e2ed957b90d79b4ddd8f9d889",
+    "url": "/wouldYouRather-app/static/media/udacity.1fc43d1e.png"
+  }
+]);
